7916a51f55e4e152f966e878c4fdcb5edf0310ee
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-09-18 15:23:13 -0700

    passing arguments 'Hello', 'World' to args from command line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	
+		System.out.println("Number of args: " + args.length);
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i] + "");
+		}
 	}
 
 }
